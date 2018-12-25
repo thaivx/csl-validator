797d75c0b9e529bc332b0e34b459c64ca39242aa
dalfod vu <dalfod.vu@contemi.com.vn>
2018-12-25 18:01:48 +0700

    update validator

--- index.js
+++ index.js
@@ -1,4 +1,55 @@
-module.exports = function fieldValidate(answer, question) {
+function validateAll(questionBlocks) {
+    let invalids = {};
+    if (answerBlocks) {
+        _.each(Object.keys(answerBlocks), key => {
+
+            if (!questionBlocks.hasOwnProperty(key)) {
+                return;
+            }
+
+            let questions = questionBlocks[key];
+            let answers = answerBlocks[key];
+            stepValidationResult = stepValidate(answers, questions);
+            stepValidationResult === true || (invalids[key] = stepValidationResult);
+        });
+    }
+    return invalids;
+}
+
+function stepValidate(answers, questions) {
+    if (_.isEmpty(answers)) return true;
+
+    let errors = {};
+    _.each(Object.keys(questions), (key) => {
+        let answer = answers[key];
+        let question = questions[key];
+        if (question.requireQuestion) {
+            if (answers[question.requireQuestion] !== question.requireValue) {
+                delete answers[key];
+                return false;
+            }
+        }
+
+        let errorText = validateField(answer, question);
+        if (errorText && typeof errorText === 'string') {
+            errors[key] = errorText;
+        }
+
+        if (question.type === 'repeat' && Array.isArray(answer)) {
+            answer.filter((asws, i) => {
+                let itemErr = stepValidate(asws, question.questions);
+                if (itemErr !== true) {
+                    (errors[key] && _.isObject(errors[key])) || (errors[key] = {});
+                    return errors[key][i] = itemErr;
+                }
+            });
+        }
+    });
+
+    return _.isEmpty(errors) || errors;
+}
+
+function validateField(answer, question) {
     let validations = question.validations;
 
     // Check if no answer given
@@ -30,4 +81,10 @@ module.exports = function fieldValidate(answer, question) {
 
     // Validation for type and some special conditions such as min, max, email, number ...
     return require('./' + question.type)(answer, validations);
+}
+
+module.exports = {
+    validateAll,
+    validateField
 };
+
